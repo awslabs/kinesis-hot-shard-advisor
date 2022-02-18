80bc7f1fb237c0fe31b030a50d7dac558dff81b7
Buddhike de Silva <buddhiks@amazon.com>
2022-02-18 13:03:33 +1100

    ascii graph concept

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/cheggaaa/pb v1.0.29
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/color v1.13.0 // indirect
+	github.com/guptarohit/asciigraph v0.5.2 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
--- ingress.go
+++ ingress.go
@@ -5,37 +5,42 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
+	"github.com/guptarohit/asciigraph"
 )
 
 type ingress struct {
 	min   int64
-	usage []float64
+	max   int64
+	usage map[string][]float64
 }
 
 func (i *ingress) Aggregate(shardID string, record *types.Record) {
+	if _, ok := i.usage[shardID]; !ok {
+		i.usage[shardID] = make([]float64, int(i.max-i.min))
+	}
 	an := record.ApproximateArrivalTimestamp.Round(time.Second).Unix()
 	// At this point we have t which is a value between min and max
 	// seconds in our series
 	// Use the formula an = a + (n – 1)d to workout n
 	// in this case d = 1 because we aggregate data one second intervals
 	n := (an - i.min) + 1
-	i.usage[n] = i.usage[n] + float64(len(record.Data))
+	i.usage[shardID][n] = i.usage[shardID][n] + float64(len(record.Data))
 }
 
 func (i *ingress) Print(shardTree map[string][]string, limit int) {
-	for idx, bps := range i.usage {
-		if bps > 0 {
-			fmt.Printf("%d - %f bytes\n", int64(idx)+i.min, bps)
-		}
+	for shardID, data := range i.usage {
+		g := asciigraph.Plot(data, asciigraph.Width(200), asciigraph.Height(10))
+		fmt.Println(shardID)
+		fmt.Println(g)
 	}
 }
 
 func newIngress(start, end time.Time) *ingress {
 	min := start.Round(time.Second).Unix()
 	max := end.Round(time.Second).Unix()
-	d := max - min
 	return &ingress{
 		min:   min,
-		usage: make([]float64, d),
+		max:   max,
+		usage: make(map[string][]float64),
 	}
 }
