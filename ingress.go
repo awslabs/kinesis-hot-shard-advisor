6d1c2378a80e1e336b3273f251e41eb9ab8abd3b
Masudur Rahaman Sayem <masudurs@amazon.com>
2022-02-23 16:31:08 +1100

    condition to print while it is more than 0.95mb

--- ingress.go
+++ ingress.go
@@ -16,21 +16,30 @@ type ingress struct {
 func (i *ingress) Aggregate(shardID string, record *types.Record) {
 	if _, ok := i.usage[shardID]; !ok {
 		i.usage[shardID] = make([]float64, int(i.max-i.min))
+
 	}
 	an := record.ApproximateArrivalTimestamp.Round(time.Second).Unix()
+	//an := record.ApproximateArrivalTimestamp.Round(time.Second).UTC()
+
 	// At this point we have t which is a value between min and max
 	// seconds in our series
 	// Use the formula an = a + (n – 1)d to workout n
 	// in this case d = 1 because we aggregate data one second intervals
 	n := (an - i.min) + 1
+
 	i.usage[shardID][n] = i.usage[shardID][n] + float64(len(record.Data))
+
 }
 
 func (i *ingress) Print(shardTree map[string][]string, limit int) {
+	//950000 = 0.95 MB. to test run the condition with 141 (byte)
 	for shardID, data := range i.usage {
 		fmt.Println(shardID)
 		for o, v := range data {
-			fmt.Printf("%s %f B/sec\n", time.Unix(i.min+int64(o), 0).Format("15:04:01"), v)
+			if v > 950000 {
+				fmt.Printf("%s %f B/sec\n", time.Unix(i.min+int64(o), 0).Format("15:04:01"), v)
+			}
+
 		}
 	}
 }
