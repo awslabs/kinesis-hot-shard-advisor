02b8ed5aa23a1a9bcbee260f33f34a677ee55f59
Buddhike de Silva <buddhiks@amazon.com>
2022-05-12 16:26:02 +1000

    Cosmetic changes

--- cmd.go
+++ cmd.go
@@ -268,7 +268,7 @@ func (i *cmd) generateReport(stats map[string]map[string]interface{}) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf(color.YellowString(": %s ", fname))
+	fmt.Print(color.YellowString(": %s ", fname))
 }
 
 func (c *cmd) ensureEFOConsumer(ctx context.Context) (*string, *string, error) {
