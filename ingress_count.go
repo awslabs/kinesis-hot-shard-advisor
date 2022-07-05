c37c90aa7549fad70dda1eb4a2d27a657f596807
Buddhike de Silva <buddhiks@amazon.com>
2022-07-05 15:18:31 +1000

    Feat: Ingress count/sec report
    This change introduces a new feature to report number of
    records written per second.
    
    Closes #2

---
+++ ingress_count.go
@@ -0,0 +1,61 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+package main
+
+import (
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/service/kinesis/types"
+)
+
+// ingressCount is an Aggregator to count number of records
+// received per second (based on ApproximateArrivalTimestamp).
+type ingressCount struct {
+	min             int64 // Start time of aggregation in Unix time format
+	max             int64 // End time of aggregation in Unix time format
+	timeSeries      []int // Store usage value for each shard as an array. Array index is the ordinal value of second within the specified range.
+	sum             int
+	maxIngressCount int
+}
+
+type ingressCountStats struct {
+	TimeSeries []int `json:"timeSeries"`
+	Sum        int   `json:"sum"`
+	Max        int   `json:"max"`
+}
+
+func (i *ingressCount) Name() string {
+	return "ingress-count"
+}
+
+func (i *ingressCount) Aggregate(record *types.Record) {
+	an := record.ApproximateArrivalTimestamp.Unix()
+	offset := (an - i.min)
+	if offset < 0 || an > i.max {
+		return
+	}
+	i.timeSeries[offset] = i.timeSeries[offset] + 1
+	i.sum = i.sum + 1
+	if i.maxIngressCount < i.timeSeries[offset] {
+		i.maxIngressCount = i.timeSeries[offset]
+	}
+}
+
+func (i *ingressCount) Result() interface{} {
+	return ingressCountStats{
+		i.timeSeries,
+		i.sum,
+		i.maxIngressCount,
+	}
+}
+
+func newIngressCount(start, end time.Time) *ingressCount {
+	min := start.Unix()
+	max := end.Unix()
+	return &ingressCount{
+		min:        min,
+		max:        max,
+		timeSeries: make([]int, int(max-min)+1),
+	}
+}
